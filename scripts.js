271d930ac7ee52ea6ef2ec35d7b5a5747b3513b7
Simcaa007 <havelkova.si@seznam.cz>
2025-10-18 21:34:04 +0200

    Sidebar

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-let nav = document.querySelector("nav");
-let tlacitko = document.querySelector(".kuk");
+const nav = document.querySelector("nav");
+const tlacitko = document.querySelector(".kuk");
 
-tlacitko.addEventListener("click", function() {
+tlacitko.addEventListener("click", () => {
   nav.classList.toggle('nav--active');
+
+  const img = tlacitko.querySelector('img');
+
+  if(nav.classList.contains('nav--active')){
+      img.src = 'svg/x-svgrepo-com.svg';
+  } else {
+      img.src = 'svg/align-justify-svgrepo-com.svg';
+  }
 });
